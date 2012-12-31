f68fbefeb7c60fd13f7e96191e6c043d49b347c7
Phil Busch <phil.busch@gmail.com>
2012-12-30 23:06:41 -0600

    Added SplashScreen and empty soundboard activity
    Got the DMX image appearing on the splash screen and created the
    activity that will be the soundboard. Need to figure out how to
    transition between the two

---
+++ code/src/com/soundboard/dmx/Soundboard.java
@@ -0,0 +1,20 @@
+package com.soundboard.dmx;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Soundboard extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soundboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_soundboard, menu);
+        return true;
+    }
+}
