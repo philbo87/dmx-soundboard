246e38a3a8c2cfca4343cb6b593df7d99795c69c
Phil Busch <phil.busch@gmail.com>
2012-12-30 23:14:51 -0600

    Got the timed transition between Splash Screen Activity and Soundboard working!

--- code/src/com/soundboard/dmx/SplashScreen.java
+++ code/src/com/soundboard/dmx/SplashScreen.java
@@ -1,20 +1,35 @@
 package com.soundboard.dmx;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class SplashScreen extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-    }
+	Handler mHandler = new Handler();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-     //   getMenuInflater().inflate(R.menu.activity_splash_screen, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+
+		mHandler.postDelayed(mLaunchTask, 3000);
+
+	}
+
+	private Runnable mLaunchTask = new Runnable() {
+		public void run() {
+			Intent i = new Intent(getApplicationContext(), Soundboard.class);
+			startActivity(i);
+			finish();
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// getMenuInflater().inflate(R.menu.activity_splash_screen, menu);
+		return true;
+	}
 }
